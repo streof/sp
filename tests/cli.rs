89b5a86c6ea85812343b3e6911ad83ea65a643fd
nova <10353426+streof@users.noreply.github.com>
2020-03-25 20:00:35 +0100

    Better format and minor change to a test case

--- tests/cli.rs
+++ tests/cli.rs
@@ -1,11 +1,12 @@
 use assert_cmd::prelude::*;
 use predicates::prelude::*;
+use std::error::Error;
 use std::io::Write;
 use std::process::Command;
 use tempfile::NamedTempFile;
 
 #[test]
-fn file_doesnt_exist() -> Result<(), Box<dyn std::error::Error>> {
+fn file_doesnt_exist() -> Result<(), Box<dyn Error>> {
     let mut cmd = Command::cargo_bin("grrs")?;
     cmd.arg("foobar").arg("test/file/doesnt/exists");
     cmd.assert()
@@ -15,23 +16,29 @@ fn file_doesnt_exist() -> Result<(), Box<dyn std::error::Error>> {
 }
 
 #[test]
-fn find_content_in_file() -> Result<(), Box<dyn std::error::Error>> {
+fn find_content_in_file() -> Result<(), Box<dyn Error>> {
     let mut file = NamedTempFile::new()?;
-    writeln!(file, "A test\nActual content\nMore content\nAnother test")?;
+    writeln!(
+        file,
+        "A test\nActual content\r\nMore content\nA\x00nother test"
+    )?;
 
     let mut cmd = Command::cargo_bin("grrs")?;
     cmd.arg("test").arg(file.path());
     cmd.assert()
         .success()
-        .stdout(predicate::str::contains("test\nAnother test"));
+        .stdout(predicate::str::contains("test\nA\x00nother test"));
 
     Ok(())
 }
 
 #[test]
-fn empty_string_search() -> Result<(), Box<dyn std::error::Error>> {
+fn empty_string_search() -> Result<(), Box<dyn Error>> {
     let mut file = NamedTempFile::new()?;
-    writeln!(file, "A test\nActual content\nMore content\nAnother test")?;
+    writeln!(
+        file,
+        "A test\nActual content\nMore content\nA\x00nother test"
+    )?;
 
     let mut cmd = Command::cargo_bin("grrs")?;
     cmd.arg("").arg(file.path());
