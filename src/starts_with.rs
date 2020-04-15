f5fa059d94e19bad7b9fa31632ad1cf11f926a63
nova <10353426+streof@users.noreply.github.com>
2020-04-15 18:50:10 +0200

    Finalize implementation -s flag
    The new -s flag now also works correctly when -n is provided. Unit
    tests are added. This commit completes the implementation

--- src/starts_with.rs
+++ src/starts_with.rs
@@ -4,10 +4,10 @@ use bstr::{io::BufReadExt, ByteSlice};
 use std::io::BufRead;
 
 trait StartsWith {
-    // fn no_line_number(&mut self) -> SearcherResult;
-    // fn no_line_number_caseless(&mut self) -> SearcherResult;
-    // fn no_line_number_max_count(&mut self) -> SearcherResult;
-    // fn no_line_number_caseless_max_count(&mut self) -> SearcherResult;
+    fn no_line_number(&mut self) -> SearcherResult;
+    fn no_line_number_caseless(&mut self) -> SearcherResult;
+    fn no_line_number_max_count(&mut self) -> SearcherResult;
+    fn no_line_number_caseless_max_count(&mut self) -> SearcherResult;
     fn line_number(&mut self) -> SearcherResult;
     fn line_number_caseless(&mut self) -> SearcherResult;
     fn line_number_max_count(&mut self) -> SearcherResult;
@@ -19,6 +19,115 @@ pub trait StartsWithSearch {
 }
 
 impl<'a, R: BufRead> StartsWith for Searcher<'a, R> {
+    fn no_line_number(&mut self) -> SearcherResult {
+        let (reader, pattern) = (&mut self.reader, &self.matcher.pattern);
+
+        let mut sir = SearchInnerResult::default();
+
+        reader.for_byte_line_with_terminator(|line| {
+            if line.contains_str(pattern) {
+                sir.check_and_store_nln(pattern, line, check_starts_with);
+            }
+            Ok(true)
+        })?;
+
+        Self::ret_searcher_result(sir.matches, LineNumbers::None)
+    }
+
+    fn no_line_number_caseless(&mut self) -> SearcherResult {
+        let (reader, pattern) = (&mut self.reader, &self.matcher.pattern);
+
+        let mut sir = SearchInnerResult::default();
+
+        reader.for_byte_line_with_terminator(|line| {
+            if line.is_ascii() {
+                let line_lower = line.to_ascii_lowercase();
+                if line_lower.contains_str(pattern) {
+                    sir.check_and_store_separate_nln(pattern, &line_lower, line, check_starts_with);
+                }
+            } else {
+                let mut buf = Default::default();
+                line.to_lowercase_into(&mut buf);
+                if buf.contains_str(pattern) {
+                    sir.check_and_store_separate_nln(pattern, &buf, line, check_starts_with);
+                }
+            }
+            Ok(true)
+        })?;
+
+        Self::ret_searcher_result(sir.matches, LineNumbers::None)
+    }
+
+    fn no_line_number_max_count(&mut self) -> SearcherResult {
+        let (reader, pattern, mut matches_left) = (
+            &mut self.reader,
+            &self.matcher.pattern,
+            self.matcher.config.max_count.unwrap(),
+        );
+
+        let mut sir = SearchInnerResult::default();
+
+        reader.for_byte_line_with_terminator(|line| {
+            if matches_left == 0 as u64 {
+                return Ok(true);
+            }
+            if line.contains_str(pattern) {
+                sir.check_and_store_nln_max_count(
+                    pattern,
+                    line,
+                    &mut matches_left,
+                    check_starts_with,
+                );
+            }
+            Ok(true)
+        })?;
+
+        Self::ret_searcher_result(sir.matches, LineNumbers::None)
+    }
+
+    fn no_line_number_caseless_max_count(&mut self) -> SearcherResult {
+        let (reader, pattern, mut matches_left) = (
+            &mut self.reader,
+            &self.matcher.pattern,
+            self.matcher.config.max_count.unwrap(),
+        );
+
+        let mut sir = SearchInnerResult::default();
+
+        reader.for_byte_line_with_terminator(|line| {
+            if matches_left == 0 as u64 {
+                return Ok(true);
+            }
+            if line.is_ascii() {
+                let line_lower = line.to_ascii_lowercase();
+                if line_lower.contains_str(pattern) {
+                    sir.check_and_store_separate_nln_max_count(
+                        pattern,
+                        &line_lower,
+                        line,
+                        &mut matches_left,
+                        check_starts_with,
+                    );
+                }
+            } else {
+                let mut buf = Default::default();
+                line.to_lowercase_into(&mut buf);
+                if buf.contains_str(pattern) {
+                    sir.check_and_store_separate_nln_max_count(
+                        pattern,
+                        &buf,
+                        line,
+                        &mut matches_left,
+                        check_starts_with,
+                    );
+                }
+            }
+            Ok(true)
+        })?;
+
+        Self::ret_searcher_result(sir.matches, LineNumbers::None)
+    }
+
     fn line_number(&mut self) -> SearcherResult {
         let (reader, pattern) = (&mut self.reader, &self.matcher.pattern);
 
@@ -161,16 +270,14 @@ impl<'a, R: BufRead> StartsWithSearch for Searcher<'a, R> {
         );
 
         match (no_line_number, ignore_case, max_count) {
-            // (true, true, Some(_)) => self.no_line_number_caseless_max_count(),
-            // (true, true, None) => self.no_line_number_caseless(),
-            // (true, false, Some(_)) => self.no_line_number_max_count(),
-            // (true, false, None) => self.no_line_number(),
+            (true, true, Some(_)) => self.no_line_number_caseless_max_count(),
+            (true, true, None) => self.no_line_number_caseless(),
+            (true, false, Some(_)) => self.no_line_number_max_count(),
+            (true, false, None) => self.no_line_number(),
             (false, true, Some(_)) => self.line_number_caseless_max_count(),
             (false, true, None) => self.line_number_caseless(),
             (false, false, Some(_)) => self.line_number_max_count(),
             (false, false, None) => self.line_number(),
-            // TODO: remove this line after implementing -n APIs
-            (_, _, _) => self.line_number(),
         }
     }
 }
@@ -182,7 +289,8 @@ mod tests {
     use crate::matcher::MatcherBuilder;
     use std::io::Cursor;
 
-    const LINE: &str = "again\ngain\na\x00nd, gain\n& AΓain\ngain";
+    const LINE: &str = "again\ngain\na\x00nd, gain\n& AΓain\nGain";
+    const LINE2: &str = "again\nGain\na\x00nd, gain\n& AΓain\nGain";
 
     #[test]
     fn line_number() {
@@ -239,5 +347,88 @@ mod tests {
         assert!(matches.len() == 1);
         assert_eq!(matches[0], "& AΓain".as_bytes());
         assert_eq!(line_numbers, &LineNumbers::Some(line_number_inner));
+    }
+
+    #[test]
+    fn no_line_number() {
+        let mut line = Cursor::new(LINE.as_bytes());
+        let pattern = "gain".to_owned();
+
+        let matcher = MatcherBuilder::new()
+            .ignore_case(true)
+            .no_line_number(true)
+            .starts_with(true)
+            .build(pattern);
+
+        let searcher = Searcher {
+            reader: &mut line,
+            matcher: &matcher,
+        };
+
+        let searcher_result = searcher.search_matches();
+        let search_result = searcher_result.as_ref().unwrap();
+        let matches = &search_result.matches;
+        let line_numbers = &search_result.line_numbers;
+
+        assert!(matches.len() == 3);
+        assert_eq!(matches[0], &b"gain"[..]);
+        assert_eq!(matches[1], &b"a\x00nd, gain"[..]);
+        assert_eq!(matches[2], &b"Gain"[..]);
+        assert_eq!(line_numbers, &LineNumbers::None);
+    }
+
+    #[test]
+    fn no_line_number_max_count() {
+        let mut line = Cursor::new(LINE.as_bytes());
+        let pattern = "gain".to_owned();
+
+        let matcher = MatcherBuilder::new()
+            .max_count(Some(2))
+            .no_line_number(true)
+            .starts_with(true)
+            .build(pattern);
+
+        let searcher = Searcher {
+            reader: &mut line,
+            matcher: &matcher,
+        };
+
+        let searcher_result = searcher.search_matches();
+        let search_result = searcher_result.as_ref().unwrap();
+        let matches = &search_result.matches;
+        let line_numbers = &search_result.line_numbers;
+
+        assert!(matches.len() == 2);
+        assert_eq!(matches[0], &b"gain"[..]);
+        assert_eq!(matches[1], &b"a\x00nd, gain"[..]);
+        assert_eq!(line_numbers, &LineNumbers::None);
+    }
+
+    #[test]
+    fn no_line_number_caseless_max_count() {
+        let mut line = Cursor::new(LINE2.as_bytes());
+        let pattern = "gain".to_owned();
+
+        let matcher = MatcherBuilder::new()
+            .ignore_case(true)
+            .max_count(Some(2))
+            .no_line_number(true)
+            .starts_with(true)
+            .build(pattern);
+
+        let searcher = Searcher {
+            reader: &mut line,
+            matcher: &matcher,
+        };
+
+        let searcher_result = searcher.search_matches();
+        let search_result = searcher_result.as_ref().unwrap();
+        let matches = &search_result.matches;
+        let line_numbers = &search_result.line_numbers;
+
+        assert!(matches.len() == 2);
+        assert_eq!(matches[0], &b"Gain"[..]);
+        assert_eq!(matches[1], &b"a\x00nd, gain"[..]);
+        assert_eq!(line_numbers, &LineNumbers::None);
     }
 }
