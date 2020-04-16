ee4e90bd5a8de0361f9f7dea6cdaefb19403cf62
nova <10353426+streof@users.noreply.github.com>
2020-04-16 15:30:53 +0200

    Remove duplicate function (see search_inner)

--- src/ext.rs
+++ src/ext.rs
@@ -3,20 +3,12 @@ use std::str;
 
 pub trait ByteSliceExt {
     fn trim_terminator(&self) -> BString;
-    fn check_starts_with(&self, pattern: &str) -> bool;
 }
 
 impl ByteSliceExt for [u8] {
     /// Trims line terminator and converts result to BString
     fn trim_terminator(&self) -> BString {
         self.trim_end_with(|c| c == '\n' || c == '\r').into()
-    }
-
-    /// Checks if line contains words starting with provided pattern.
-    fn check_starts_with(&self, pattern: &str) -> bool {
-        self.trim_terminator()
-            .split_str(" ")
-            .any(|word| word.starts_with_str(pattern))
     }
 }
 
